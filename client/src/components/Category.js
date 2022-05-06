af88bc248424a422ccbfe57d4a268c3ffdc453ba
mpd253 <82489360+mpd253@users.noreply.github.com>
2022-05-06 16:11:54 +0900

    Category Title 규격 설정

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Drawer } from 'antd';
 import 'antd/dist/antd.css';
 import { MenuOutlined } from '@ant-design/icons';
+import styled from 'styled-components';
+
+
 
 const Category = () => {
   const [visible, setVisible] = useState(false);
@@ -18,7 +21,8 @@ const Category = () => {
     <>
       <MenuOutlined style={{ marginTop:10 }} onClick={showDrawer}> </MenuOutlined>
 
-      <Drawer title="전체 카테고리" placement="right" onClose={onClose} visible={visible}>
+      <Drawer title={<div style={{height:"48px", margin:"auto", paddingTop:13, fontSize:"20px"}}> 전체 카테고리 </div>} placement="right" onClose={onClose} visible={visible}>
+        
         <p>전자기기</p>
         <p>의류</p>
         <p>악세서리</p>
