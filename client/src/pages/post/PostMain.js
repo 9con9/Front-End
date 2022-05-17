f3b98cbbd95d0e4fac1809a942e17b94428efc75
mpd253 <82489360+mpd253@users.noreply.github.com>
2022-05-17 12:05:31 +0900

    이슈페이지 업데이트

---
+++ client/src/pages/post/PostMain.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostList from './PostList';
+
+const PostMain = props => {
+  return (
+    <>
+      <h2 align="center"> 공지사항 </h2>
+      <PostList />
+    </>
+  )
+}
+
+export default PostMain;
