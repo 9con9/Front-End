c735bf58be57a31fc59d3c8a7e3e7fbfb222a269
HongHyunKi <hk7286@naver.com>
2022-05-29 18:23:28 +0900

    db 연동

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -2,37 +2,40 @@ import styled from 'styled-components';
 import { Card } from 'antd';
 import 'antd/dist/antd.css';
 
-import { FormOutlined, EnvironmentOutlined, PicCenterOutlined } from '@ant-design/icons';
+import { FormOutlined, EnvironmentOutlined, CommentOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
 function ItemCard(props) {
+
     return (
         <Card
             hoverable
             style={{ width: 240 }}
             cover={
-                <ImageCard
-                    style={{
-                        backgroundImage: `url(${props.items.imgSrc})`,
-                        backgroundPosition: 'center',
-                        backgroundsize: 'cover',
-                    }}>
-                    <InnerBox>
-                        <PlatformImg src={props.items.platform} alt="image0" />
-                    </InnerBox>
-                </ImageCard>
+                <a href={props.items.link} target='blank'>
+                    <ImageCard
+                        style={{
+                            backgroundImage: `url(${props.items.img_link})`,
+                            backgroundPosition: 'center',
+                            backgroundsize: 'cover',
+                        }}>
+                        {/* <InnerBox>
+                        <PlafformImg src= {`./img/${props.items.plafform}.png`} alt="image0" />
+                    </InnerBox> */}
+                    </ImageCard>
+                </a>
             }>
-
             <Meta>
                 style={{ height: "150px" }}
             </Meta>
 
             <TextBox>
+                <h3 style={{ marginBottom: -5 }}> <CommentOutlined /> {props.items.plafform}</h3>
                 <h2>{props.items.price}</h2>
-                <p><FormOutlined />  {props.items.title}</p>
+                <p><FormOutlined />  {props.items.name}</p>
                 <p><EnvironmentOutlined />  {props.items.address}</p>
-                <h4>{props.items.time}</h4>
+                <h4>{props.items.upload_time}</h4>
             </TextBox>
         </Card>
     );
@@ -51,7 +54,7 @@ const InnerBox = styled.div`
     border-radius:4px;
 `;
 
-const PlatformImg = styled.img`
+const PlafformImg = styled.img`
     margin-top: 5px;
     margin-left: 5px;
     width: 40px;
