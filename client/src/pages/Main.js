4f843afc3c962b522f32723654a413f6a6d1255f
PARKSUNGHYUN1 <54789331+PARKSUNGHYUN1@users.noreply.github.com>
2022-05-30 20:28:13 +0900

    메인페이지 추가 구현

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import 'fullpage.js/vendors/scrolloverflow';
 import ReactFullpage from '@fullpage/react-fullpage';
+import {Link ,Route, Routes } from 'react-router-dom';
+import ChartPage from './ChartPage';
+import ProductPage from './ProductPage'
+import IssuePage from './IssuePage';
 
 class Mainpage extends React.Component {
-    onLeave(origin, destination, direction) {
+    onLeave(origin, destination, directicon) {
       console.log("Leaving section " + origin.index);
     }
     afterLoad(origin, destination, direction) {
@@ -12,42 +16,97 @@ class Mainpage extends React.Component {
     }
     render() {
       return (
-        <ReactFullpage
-          scrollOverflow={true}
-          navigation={true}
-          navigationPosition='right'
-          onLeave={this.onLeave.bind(this)}
-          afterLoad={this.afterLoad.bind(this)}
-          render={({ state, fullpageApi }) => {
-            return (
-              <div id="fullpage-wrapper">
-                <div className="section section1">
-                  <img src={process.env.PUBLIC_URL +'/img/pic5.jpg'} style={{width:"100%", height:"722px"}} />
-                </div>
-                <div className="section">
-                  <div className="slide">
-                    <img src={process.env.PUBLIC_URL +'/img/pic2.jpg'} style={{width:"100%", height:"722px"}}/>
+        <Container>
+          
+            <ReactFullpage
+              scrollOverflow={true}
+              navigation={true}
+              navigationPosition='right'
+              onLeave={this.onLeave.bind(this)}
+              afterLoad={this.afterLoad.bind(this)}
+              render={({ state, fullpageApi }) => {
+                return (
+                  <div id="fullpage-wrapper">
+                    <div className="section">
+                      <img src={process.env.PUBLIC_URL +'/img/pic4.jpg'} 
+                      style={{width:"100%", height:"100%", background: 'url(/img/pic4.jpg)'}}/>
+                      <div style={{position: "absolute", zIndex:5}}>
+                        <p style={{
+                        zIndex:5, fontWeight:'normal',marginTop:"-550px", fontWeight: 'bold',fontSize:"30px", color:"#e4e8eb", marginLeft:"40px"}}>
+                          차트를 보면서 <p style={{fontSize: "30px"}}>제품의 시세를 분석해보세요.</p>
+                        </p>
+                            <Button to='/Chart' onClick="activateLasers()" variant="primary"  size="medium"style={{marginLeft:"40px", fontWeight: 'bold'}}>
+                              차트 바로가기
+                            </Button>
+                      </div>
+                    </div>
+                    <div className="section">
+                        <img src={process.env.PUBLIC_URL +'/img/pic2.jpg'} 
+                        style={{width:"100%", height:"100%", zIndex:0}}/>
+                        <div style={{position: "absolute", zIndex:5}}>
+                          <p style={{
+                            zIndex:5,fontWeight: 'normal', marginTop:"-550px", fontWeight: 'bold', fontSize:"30px", color:"#e4e8eb", marginLeft:"40px"}}>
+                              한번에 중고제품가격을<p style={{fontSize:"30px"}}>비교해 보세요.</p>
+                            </p>
+                              <Button to='/Product' style={{marginLeft:"40px", fontWeight: 'bold'}}>
+                                마켓 바로가기
+                              </Button>
+                        </div>
+                    </div>
+                    <div className="section">
+                      <img src={process.env.PUBLIC_URL +'/img/pic3.jpg'}
+                       style={{width:"100%", height:"100%"}}/>
+                      <div style={{position: "absolute", zInd기x:5}}>
+                        <p style={{
+                          zIndex:5, fontWeight:'normal', marginTop:"-550px", fontWeight: 'bold', fontSize:"30px", color:"#e4e8eb", marginLeft:"40px"}}>
+                            다양한 사기사례를<p style={{fontSize:"30px"}}>확인해 보세요.</p>
+                        </p>
+                          <Button to='/issue' style={{marginLeft:"40px", fontWeight:"bold"}}>
+                            이슈페이지 가기
+                          </Button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="slide">
-                    <h3>설명2</h3>
-                  </div>
-                  <div className="slide">
-                    <h3>설명3</h3>
-                  </div>
-                </div>
-                <div className="section active">
-                  <img src={process.env.PUBLIC_URL +'/img/pic3.jpg'} style={{width:"100%", height:"722px"}}/>
-                </div>
-              </div>
-            );
-          }}
-        />
+                );
+              }}
+            />
+          
+        </Container>  
       );
     }
   }
-  
+
+<main>
+  <Route exact path = "/Chart" Component={ChartPage} />
+  <Route exact path='/Prodcut' Component={ProductPage}/>
+  <Route exact path='/issue' Component={IssuePage}/>
+</main>
+
+
   export default Mainpage
 
-  const h3 = styled.div`
-  justify-content: center;
+  const Container = styled.div`
+  display: flex;
   `
+
+  export const Button = styled(Link)`
+    color: white;
+    border-radius: 50px;
+    background: ${({primary})=> (primary ? '#6A67CE' : '#232323')};
+    white-space: nowrap;
+    padding: ${({big})=> (big ? '14px 48px' : '12px 30px')};
+    color: ${({dark})=> (dark ? '#EEEEE' : '#fff')};
+    font-size: ${({fontBig})=> (fontBig ? '20px' : '16px')};
+    outline: none;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.3s ease-in-out;
+    text-decoration-line: none;
+    &:hover {
+        transition: all 0.3s ease-in-out;
+        background: ${({primary})=> (primary ? '#5B4FE8': '#f56217' )}; //뒤에꺼
+    }
+`
